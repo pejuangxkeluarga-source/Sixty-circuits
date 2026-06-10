7811a26345bec8aee35e49bbe73ff943aff3d184
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-06-10 11:08:05 +0700

    Update projects.js

--- js/data/projects.js
+++ js/data/projects.js
@@ -2,7 +2,7 @@ var soixanteprojects = [{
       url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=24ac8f74b6b0941716fbfc37866447b0',
       type: 'internet'
 }, {
-      url: 'https://www.effectivecpmnetwork.com/ad1w33ifa?key=4674df618a373e1209af73eac3e26f9b',
+      url: 'https://www.effectivecpmnetwork.com/ad1w33ifa?key=cc6e323bb395d7ed6390dcb37a44b102',
       type: 'internet'
 }
 //{url:''},
