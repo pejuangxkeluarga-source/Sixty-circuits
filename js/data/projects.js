38bff382fe4305bc09b6ab8910b70a5619e755de
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-07-14 19:25:45 +0700

    Update projects.js

--- js/data/projects.js
+++ js/data/projects.js
@@ -2,7 +2,7 @@ var soixanteprojects = [{
       url: 'https://bjdbai.hubbusty.com/s/5de5f8eacbde1?subsource=DIABLO&ext_click_id=DIABLO&sub1=DIABLO',
       type: 'internet'
 }, {
-      url: 'https://eooqz.lnventivedates.net/?utm_source=da57dc555e50572d&ban=other&j5=1&j6=1&s1=15810&s2=2209769&click_id=jipe',
+      url: 'https://bjdbai.hubbusty.com/s/5de5f8eacbde1?subsource=DIABLO&ext_click_id=DIABLO&sub1=DIABLO',
       type: 'internet'
 }
 //{url:''},
