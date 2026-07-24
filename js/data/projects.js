cf8ffd368fd80b5cab180e36a9a36b0805d8beee
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-07-24 07:22:37 +0700

    Update projects.js

--- js/data/projects.js
+++ js/data/projects.js
@@ -1,8 +1,20 @@
 var soixanteprojects = [{
-      url: 'https://bjdbai.hubbusty.com/s/5de5f8eacbde1?subsource=DIABLO&ext_click_id=DIABLO&sub1=DIABLO',
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=24ac8f74b6b0941716fbfc37866447b0',
       type: 'internet'
 }, {
-      url: 'https://bjdbai.hubbusty.com/s/5de5f8eacbde1?subsource=DIABLO&ext_click_id=DIABLO&sub1=DIABLO',
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=b827158e5f98395b8020b49450758cf0',
+      type: 'scenographie'
+}, {
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=24ac8f74b6b0941716fbfc37866447b0',
+      type: 'application'
+}, {
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=b827158e5f98395b8020b49450758cf0',
+      type: 'enseignement'
+}, {
+      url: 'https://omg10.com/4/8821538',
+      type: 'scenographie'
+}, {
+      url: 'https://omg10.com/4/8821538',
       type: 'internet'
 }
 //{url:''},
